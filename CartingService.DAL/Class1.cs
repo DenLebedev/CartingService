df7c3b901ca8b90acc5837a5e049b0c045cfd669
Den V. Lebedev <dvl@inbox.ru>
2023-03-20 08:19:13 +0300

    Add project files.

---
+++ CartingService.DAL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CartingService.DAL
+{
+    public class Class1
+    {
+    }
+}
---
+++ CartingService.GlobalEntity/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CartingService.GlobalEntity
+{
+    public class Class1
+    {
+    }
+}
